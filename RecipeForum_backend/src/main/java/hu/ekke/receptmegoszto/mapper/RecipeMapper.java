e946358628bba26177e303c6a63a093273d30dc9
pataricza1987 <pataricza1987@gmail.com>
2025-10-29 18:46:45 +0100

    feature/recipe-api: getting recipe

--- RecipeForum_backend/src/main/java/hu/ekke/receptmegoszto/mapper/RecipeMapper.java
+++ RecipeForum_backend/src/main/java/hu/ekke/receptmegoszto/mapper/RecipeMapper.java
@@ -1,8 +1,6 @@
 package hu.ekke.receptmegoszto.mapper;
 
-import hu.ekke.receptmegoszto.domain.Category;
 import hu.ekke.receptmegoszto.domain.Ingredient;
-import hu.ekke.receptmegoszto.domain.Material;
 import hu.ekke.receptmegoszto.domain.Recipe;
 import hu.ekke.receptmegoszto.dto.IngredientDto;
 import hu.ekke.receptmegoszto.dto.RecipeDto;
@@ -29,12 +27,4 @@ public interface RecipeMapper {
     @Mapping(target = "recipe", ignore = true)
     @Mapping(target = "material", ignore = true)
     Ingredient ingredientToEntity(IngredientDto dto);
-
-    default Long categoryToId(Category category) {
-        return category != null && category.getId() != null ? category.getId().longValue() : null;
-    }
-
-    default Long materialToId(Material material) {
-        return material != null && material.getId() != null ? material.getId().longValue() : null;
-    }
 }
