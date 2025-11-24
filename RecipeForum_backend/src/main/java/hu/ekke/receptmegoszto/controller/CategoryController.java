2eac99ff60a053692a47b926f5e6e9fe6995f3b5
pataricza1987 <pataricza1987@gmail.com>
2025-11-24 19:33:05 +0100

    feature/get all material and ingredients: endpoints

---
+++ RecipeForum_backend/src/main/java/hu/ekke/receptmegoszto/controller/CategoryController.java
@@ -0,0 +1,26 @@
+package hu.ekke.receptmegoszto.controller;
+
+import hu.ekke.receptmegoszto.dto.CategoryDto;
+import hu.ekke.receptmegoszto.service.CategoryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    private final CategoryService service;
+
+    public CategoryController(CategoryService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<CategoryDto> getAll() {
+        return service.getAll();
+    }
+}
+
---
+++ RecipeForum_backend/src/main/java/hu/ekke/receptmegoszto/controller/MaterialController.java
@@ -0,0 +1,26 @@
+package hu.ekke.receptmegoszto.controller;
+
+import hu.ekke.receptmegoszto.dto.MaterialDto;
+import hu.ekke.receptmegoszto.service.MaterialService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/material")
+public class MaterialController {
+
+    private final MaterialService service;
+
+    public MaterialController(MaterialService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<MaterialDto> getAll() {
+        return service.getAll();
+    }
+}
+
