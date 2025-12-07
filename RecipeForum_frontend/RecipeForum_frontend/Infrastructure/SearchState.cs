90d1da1017ca4fcc2636f144112ae34b42908380
rekorin <kukoriq@gmail.com>
2025-12-08 00:04:15 +0100

    Add SearchState service for cross-component communication

---
+++ RecipeForum_frontend/RecipeForum_frontend/Infrastructure/SearchState.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace RecipeForum_frontend.Infrastructure
+{
+    public class SearchState
+    {
+
+        private string _currentSearchTerm = string.Empty;
+
+        public string CurrentSearchTerm
+        {
+            get => _currentSearchTerm;
+            private set
+            {
+                if (_currentSearchTerm != value)
+                {
+                    _currentSearchTerm = value;
+                    NotifyStateChanged();
+                }
+            }
+        }
+
+        public void SetSearchTerm(string newTerm)
+        {
+            CurrentSearchTerm = newTerm;
+        }
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
+
