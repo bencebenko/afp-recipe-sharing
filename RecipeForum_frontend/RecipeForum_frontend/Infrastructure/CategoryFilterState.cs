8d4b79971a97fb6cf65d2acd520b35d0bf5d4ff0
rekorin <kukoriq@gmail.com>
2025-12-07 17:57:50 +0100

    Adding service for global state management of the category filtering

---
+++ RecipeForum_frontend/RecipeForum_frontend/Infrastructure/CategoryFilterState.cs
@@ -0,0 +1,32 @@
+﻿namespace RecipeForum_frontend.Infrastructure
+{
+    public class CategoryFilterState
+    {
+        public event Action OnChange;
+        public event Action OnFilterReset;
+        private bool _showFilter = false;
+
+        public bool ShowFilter
+        {
+            get => _showFilter;
+            set
+            {
+                if (_showFilter != value)
+                {
+                    _showFilter = value;
+                    NotifyStateChanged();
+                }
+            }
+        }
+        public void ToggleFilter()
+        {
+            ShowFilter = !ShowFilter;
+        }
+        private void NotifyStateChanged() => OnChange?.Invoke();
+        public void ResetFilter()
+        {
+            ShowFilter = false;
+            OnFilterReset?.Invoke();
+        }
+    }
+}
